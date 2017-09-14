95f766d24331955ca64e54c2b9b3bf11b2386321
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-09-14 10:09:33 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
 export function manageFriends(state, action){
+	switch (action.type) {
+		case 'ADD_FRIEND':
+			return {friends: [...state.friends, {name: action.friend.name, hometown: action.friend.hometown, id: action.friend.id}]}
+		case 'REMOVE_FRIEND':
+			return {friends: state.friends.filter(friend => friend.id !==  action.id)}
+
+		default:
+			return state
+	}
 }
